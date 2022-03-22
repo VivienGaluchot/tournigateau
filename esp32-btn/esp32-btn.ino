d771b1934a79ede3be7176eed5c9a534cf69599b
Vivien Galuchot <vivien.galuchot@gmail.com>
2022-03-22 20:04:29 +0100

    ramp mtr

--- esp32-btn/esp32-btn.ino
+++ esp32-btn/esp32-btn.ino
@@ -22,7 +22,6 @@ const int mtrMs2Pin = 16;
 
 // stepper
 
-const double mtrPwmfreq = 10000;
 const int mtrPwmResolution = 2;
 const int mtrPwmChannel = 0;
 
@@ -36,15 +35,14 @@ void mtrInit() {
     // disable
     digitalWrite(mtrEnPin, HIGH);
 
-    // 1/16 microstepping
-    digitalWrite(mtrMs1Pin, HIGH);
-    digitalWrite(mtrMs2Pin, HIGH);
+    // 1/8 microstepping
+    digitalWrite(mtrMs1Pin, LOW);
+    digitalWrite(mtrMs2Pin, LOW);
     
     // configure PWM
-    ledcSetup(mtrPwmChannel, mtrPwmfreq, mtrPwmResolution);
+    ledcSetup(mtrPwmChannel, 1, mtrPwmResolution);
     ledcWrite(mtrPwmChannel, 0);
 
-    ledcAttachPin(ledPin, mtrPwmChannel);
     ledcAttachPin(mtrStepPin, mtrPwmChannel);
 }
 
@@ -57,13 +55,58 @@ void mtrSet(bool isEnabled, float rpm) {
         } else {
             digitalWrite(mtrDirPin, LOW);
         }
-
+        
+        // TODO convert rmp to freq
+        float mtrPwmfreq = rpm;
+        ledcSetup(mtrPwmChannel, mtrPwmfreq, mtrPwmResolution);
         ledcWrite(mtrPwmChannel, 1);
     } else {
         ledcWrite(mtrPwmChannel, 0);
     }
 }
 
+long mtrRampStartTimeInMs = 0;
+long mtrRampDeltaTimeInMs = 0;
+float mtrRampStartFreq = 0;
+float mtrRampDeltaFreq = 0;
+bool mtrIsRampDone = false;
+
+void mtrRampInit(long timeInMs, float fromRmp, float toRmp, float rmpPerSec) {
+    // TODO convert rmp to freq
+    float fromFreq = fromRmp;
+    float toFreq = toRmp;
+
+    mtrRampStartTimeInMs = timeInMs;
+    mtrRampStartFreq = fromFreq;
+
+    mtrRampDeltaFreq = toFreq - fromFreq;
+    mtrRampDeltaTimeInMs = (1000 * mtrRampDeltaFreq) / rmpPerSec;
+
+    mtrIsRampDone = false;
+    mtrSet(true, fromRmp);
+
+    Serial.print("ramp: ");
+    Serial.print(mtrRampDeltaFreq);
+    Serial.print(" Hz over ");
+    Serial.print(mtrRampDeltaTimeInMs);
+    Serial.println(" ms");
+}
+
+void mtrRampCycle(long timeInMs) {
+    if (mtrIsRampDone) {
+        return;
+    }
+
+    float rate = (float)(timeInMs - mtrRampStartTimeInMs) / mtrRampDeltaTimeInMs;
+    float freq = 0;
+    if (rate > 0 && rate < 1) {
+        freq = mtrRampStartFreq + (mtrRampDeltaFreq * rate);
+    } else if (rate >= 1) {
+        freq = mtrRampStartFreq + mtrRampDeltaFreq;
+        mtrIsRampDone = true;
+    }
+    ledcWriteTone(mtrPwmChannel, freq);
+}
 
 // button
 
@@ -107,22 +150,44 @@ void setup() {
     Serial.println("started");
 }
 
+const long logPeriodInMs = 1000;
+long lastLogInMs = 0;
+bool isRunning = false;
+
 void loop() {
+    long timeInMs = millis();
     btnRead();
 
     if (btnHasSwitched) {
         Serial.print("button ");
         Serial.println(btnIsPressed);
-
-        if (btnIsPressed) {
-            mtrSet(true, 1);
-        } else {
-            mtrSet(false, 0);
+        if (!btnIsPressed) {
+            if (isRunning) {
+                mtrSet(false, 0);
+                isRunning = false;
+            } else {
+                mtrRampInit(timeInMs, 200, 5000, 10000);
+                isRunning = true;
+            }
         }
-
-        Serial.print("freq ");
-        Serial.println(ledcReadFreq(mtrPwmChannel));
     }
 
-    // digitalWrite(ledPin, btnIsPressed);
+    if (isRunning) {
+        mtrRampCycle(timeInMs);
+    }
+
+    if ((timeInMs - lastLogInMs) > logPeriodInMs) {
+        Serial.print("[");
+        Serial.print(timeInMs);
+        Serial.print("]");
+        Serial.print(" freq ");
+        Serial.print(ledcReadFreq(mtrPwmChannel));
+        Serial.print(" ramp ");
+        Serial.println(!mtrIsRampDone);
+        lastLogInMs += logPeriodInMs;
+    }
+
+    digitalWrite(ledPin, isRunning);
+
+    delay(10);
 }
